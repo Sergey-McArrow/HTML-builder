b1860b0e607057ec41140be547eb3235f11b6fee
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-24 19:00:11 +0400

    feat: finish 04

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const filesDir = path.join(__dirname, 'files/');
+const filesCopyDir = path.join(__dirname, 'files-copy/');
 
+fs.mkdir(filesCopyDir, { recursive: true }, err => {
+  if (err) console.log(err);
+});
+fs.readdir(filesDir, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  else {
+    files.forEach(file => {
+      if (file.isFile()) {
+        fs.copyFile(
+          path.join(filesDir, file.name),
+          path.join(filesCopyDir, file.name),
+          err => {
+            if (err) console.log(err);
+          }
+        );
+      }
+    });
+  }
+});
