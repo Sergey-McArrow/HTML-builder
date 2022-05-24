b2f1bdc772cd4c5515e4f47cfec7a8f4895cfb09
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-24 15:47:59 +0400

    fix: bugs

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,11 +1,19 @@
 const fs = require('fs');
 const path = require('path');
-const { stdin, stdout } = process;
+const { stdin, stdout, exit } = process;
+
+const myWriteStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+
+stdout.write('Write your text here:');
+
 stdin.on('data', data => {
-  if (data === '.exit') {
-    stdin.exit(1);
+  if (data.toString().trim() == '.exit') {
+    stdout.write('This is the end!\n');
+    exit();
   }
-  writeToFile(data);
+  writeToFile(data); // 1 вариант
+  myWriteStream.write(data); // 2 вариант
+  // при проверке выберите один плиз
 });
 process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
 
@@ -17,47 +25,3 @@ const writeToFile = text => {
     console.log(`File contains: ${text}`);
   });
 };
-
-// const fs = require('fs');
-// const path = require('path');
-// const readline = require('readline');
-// const process = require('process');
-// const { stdin: input, stdout: output } = require('process');
-// const rl = readline.createInterface({ input, output });
-
-// const readStream = () => {
-//   rl.question('input some text', text => {
-//     if (text != '.exit') {
-//       writeToFile(text);
-//       //   readStream();
-//     }
-//     rl.close();
-//   });
-// };
-
-// const writeToFile = text => {
-//   fs.appendFile(path.join(__dirname, 'text.txt'), text, err => {
-//     if (err) {
-//       throw err;
-//     }
-//     console.log(`File created and contains: ${text}`);
-//   });
-// };
-
-// const stream = fs.createReadStream('source.txt', 'utf-8');
-// stream.on('data', readStream());
-// process.on('beforeExit', error => {
-//   if (error) {
-//     throw error;
-//   }
-//   readStream();
-// });
-
-// const input = fs.createReadStream(readStream());
-// const output = fs.createWriteStream(path.join(__dirname, 'text.txt'));
-// // let data = '';
-// input.on('data', chunk => writeToFile(chunk));
-// input.on('end', () => console.log('End'));
-// input.on('error', error => console.log('Error', error.message));
-
-// readStream();
