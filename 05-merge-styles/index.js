e6ee4c7456c4ca26f548cfa472257d094ebf5e55
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-24 23:36:34 +0400

    feat: five complete

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+const myWriteStream = fs.createWriteStream(
+  path.join(__dirname, 'project-dist', 'bundle.css')
+);
+
+fs.readdir(
+  path.join(__dirname, 'styles'),
+  { withFileTypes: true },
+  (err, files) => {
+    if (err) console.log(err);
+    else {
+      files.forEach(file => {
+        if (file.name.endsWith('css')) {
+          fs.readFile(
+            path.join(__dirname, 'styles', file.name),
+            'utf-8',
+            (err, data) => {
+              if (err) console.log(err);
+              myWriteStream.write(data);
+            }
+          );
+        }
+      });
+    }
+  }
+);
