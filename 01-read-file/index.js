5d400d909f5deaebb9f63400ad177b56e7653f08
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-24 15:52:16 +0400

    fix: bugs

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,13 @@
 const path = require('path');
 const fs = require('fs');
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (err, text) => {
-  if (err) throw err;
-  console.log(text);
-});
-console.log();
+const myReadStream = fs.createReadStream(
+  path.join(__dirname, 'text.txt'),
+  'utf8'
+);
+
+myReadStream.on('data', text => console.log(text));
+
+// fs.readFile(path.join(__dirname, 'text.txt'), 'utf8', (err, text) => {
+//   if (err) throw err;
+//   console.log(text);
+// });
