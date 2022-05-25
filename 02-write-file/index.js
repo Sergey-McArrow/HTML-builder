178affca30681661672a51d809ac8532250f233d
Sergey-McArrow <sergey.mcarrow@gmail.com>
2022-05-25 10:03:45 +0400

    fix: ctrl+c

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,21 +7,21 @@ const myWriteStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
 stdout.write('Write your text here:');
 
 stdin.on('data', data => {
-  if (data.toString().trim() == '.exit') {
+  if (data.toString().trim() == 'exit') {
     stdout.write('This is the end!\n');
     exit();
   }
-  writeToFile(data); // 1 вариант
+  // writeToFile(data); // 1 вариант
   myWriteStream.write(data); // 2 вариант
   // при проверке выберите один плиз
 });
 process.on('exit', () => stdout.write('Удачи в изучении Node.js!'));
-
-const writeToFile = text => {
-  fs.appendFile(path.join(__dirname, 'text.txt'), text, err => {
-    if (err) {
-      throw err;
-    }
-    console.log(`File contains: ${text}`);
-  });
-};
+process.on('SIGINT', process.exit);
+// const writeToFile = text => {
+//   fs.appendFile(path.join(__dirname, 'text.txt'), text, err => {
+//     if (err) {
+//       throw err;
+//     }
+//     console.log(`File contains: ${text}`);
+//   });
+// };
